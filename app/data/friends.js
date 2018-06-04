69c92e214b5110cb5677791fc7af738f09aae846
sobayashi <sophieobayashi@gmail.com>
2018-06-03 21:31:17 -0700

    added more questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,24 +3,45 @@ var users = [
         "name":"Bear",
         "photo":"https://media.giphy.com/media/xNT2CcLjhbI0U/giphy.gif",
         "scores":[
+            3,
+            5,
+            3,
             1,
+            4,
+            1,
+            3,
             2,
-            3
+            4,
+            5
                 ]
     },
     {
         "name":"Chicken",
         "photo":"https://media.giphy.com/media/VofiGkwOdH2fu/200.gif",
         "scores":[
-            3,
-            3,
-            3
+            1,
+            4,
+            5,
+            1,
+            2,
+            5,
+            5,
+            4,
+            5,
+            2
                 ]
     },
     {
         "name":"Duck",
         "photo":"https://media.giphy.com/media/l0MYwX88hDzEEstnq/giphy.gif",
         "scores":[
+            1,
+            5,
+            1,
+            1,
+            2,
+            1,
+            5,
             5,
             1,
             3
